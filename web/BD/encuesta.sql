8033c53f52831e0a3a179a7a8e6eb322cca808bb
smarv2 <smarv2@hotmail.com>
2017-04-04 10:42:23 -0500

    BD

---
+++ web/BD/encuesta.sql
@@ -0,0 +1,183 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 04-04-2017 a las 17:05:35
+-- Versión del servidor: 10.1.19-MariaDB
+-- Versión de PHP: 5.6.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `encuesta`
+--
+
+DELIMITER $$
+--
+-- Procedimientos
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `st_borraEncuesta` (IN `idencuesta` INT)  BEGIN
+  DELETE FROM opcion WHERE id_pregunta IN (SELECT id_pregunta from pregunta WHERE id_encuesta = idencuesta);
+  DELETE FROM pregunta WHERE id_encuesta = idencuesta;
+  DELETE FROM encuesta where id_encuesta = idencuesta;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `st_borraOpcion` (IN `idopcion` INT)  BEGIN
+  DELETE FROM opcion WHERE id_opcion = idopcion;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `st_borraPregunta` (IN `idpregunta` INT)  BEGIN
+  DELETE FROM pregunta where id_pregunta = idpregunta;
+  DELETE FROM opcion WHERE id_pregunta = idpregunta;
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `encuesta`
+--
+
+CREATE TABLE `encuesta` (
+  `id_encuesta` int(11) NOT NULL,
+  `nombre_encuesta` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `encuesta`
+--
+
+INSERT INTO `encuesta` (`id_encuesta`, `nombre_encuesta`) VALUES
+(13, 'Autos'),
+(15, 'Motos');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `opcion`
+--
+
+CREATE TABLE `opcion` (
+  `id_opcion` int(11) NOT NULL,
+  `id_pregunta` int(11) NOT NULL,
+  `nombre_opcion` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `opcion`
+--
+
+INSERT INTO `opcion` (`id_opcion`, `id_pregunta`, `nombre_opcion`) VALUES
+(33, 32, 'BMW'),
+(34, 32, 'ACURA'),
+(35, 32, 'VW'),
+(36, 32, 'HYUNDAI'),
+(43, 35, 'Suzuki');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pregunta`
+--
+
+CREATE TABLE `pregunta` (
+  `id_pregunta` int(11) NOT NULL,
+  `id_encuesta` int(11) NOT NULL,
+  `nombre_pregunta` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `pregunta`
+--
+
+INSERT INTO `pregunta` (`id_pregunta`, `id_encuesta`, `nombre_pregunta`) VALUES
+(32, 13, 'Que marca de auto prefieres?'),
+(35, 15, 'Que marca de moto prefieres?'),
+(36, 13, 'Ã?Â¿Que color de auto te gusta mÃ¡s');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id_usuario` int(11) NOT NULL,
+  `Nombre` varchar(50) NOT NULL,
+  `Apellido` varchar(50) NOT NULL,
+  `Usuario` varchar(50) NOT NULL,
+  `Password` varchar(50) NOT NULL,
+  `Rol` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id_usuario`, `Nombre`, `Apellido`, `Usuario`, `Password`, `Rol`) VALUES
+(8, 'Mario', 'Ramirez', 'admin', '12345', 1);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `encuesta`
+--
+ALTER TABLE `encuesta`
+  ADD PRIMARY KEY (`id_encuesta`);
+
+--
+-- Indices de la tabla `opcion`
+--
+ALTER TABLE `opcion`
+  ADD PRIMARY KEY (`id_opcion`);
+
+--
+-- Indices de la tabla `pregunta`
+--
+ALTER TABLE `pregunta`
+  ADD PRIMARY KEY (`id_pregunta`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `encuesta`
+--
+ALTER TABLE `encuesta`
+  MODIFY `id_encuesta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+--
+-- AUTO_INCREMENT de la tabla `opcion`
+--
+ALTER TABLE `opcion`
+  MODIFY `id_opcion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+--
+-- AUTO_INCREMENT de la tabla `pregunta`
+--
+ALTER TABLE `pregunta`
+  MODIFY `id_pregunta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
